ff18e75944d4a80c8043b8f6c75236a09bdf7d4b
yann <y.sheja@alustudent.com>
2026-07-23 11:09:22 +0200

    comments added

--- organizer.sh
+++ organizer.sh
@@ -24,20 +24,23 @@ ORIGINAL_FILE="grades.csv"
 ARCHIVED_FILE="grades_${TIMESTAMP}.csv"
 
 if [ -f "$ORIGINAL_FILE" ];then 
-    mv "$ORIGINAL_FILE" archive/$ARCHIVED_FILE 
+    mv "$ORIGINAL_FILE" archive/$ARCHIVED_FILE #move the grades.csv into the archive folder and also rename it with the timestamp
     echo "The '$ORIGINAL_FILE' was successfully moved to 'archive/$ARCHIVED_FILE'"
 
 
 #PART 4: CREATE A NEW AND EMPTY FILE NAMED 'grades.csv' FOR THE NEXT BATCH OF GRADES
+    #'touch' creates a new and empty file if it doesn't exists for the next fresh batch of grades 
     touch $ORIGINAL_FILE
     echo "A new and empty '$ORIGINAL_FILE' was created" 
 
 #PART 5: LOG EVERYTHING INTO 'organizer.log'
 
-    echo "[$TIMESTAMP] archived '$ORIGINAL_FILE' as '$ARCHIVED_FILE'" >> organizer.log
+    echo "[$TIMESTAMP] archived '$ORIGINAL_FILE' as '$ARCHIVED_FILE'" >> organizer.log #'>>' appends to the end of the file then,
+    #creates it if it doesn't exist yet. Instead of overwriting it, so eveytimr it runs a new line is added to the history. 
     echo "Logged this action to organizer.log"
 
 else
+    # This would run if the grades.csv file did not exist when the script was started.
     echo "There was no $ORIGINAL_FILE found in the current folder. There's nothing to archive" 
     echo "[$TIMESTAMP] attempted to archive '$ORIGINAL_FILE' but it was missing." >> organizer.log
 fi 
