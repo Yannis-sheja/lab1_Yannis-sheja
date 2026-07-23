6eb0317a73ee8feddfc747e4616dc7b22fb3da98
yann <y.sheja@alustudent.com>
2026-07-23 10:09:17 +0200

    Adding organizer.log

--- organizer.sh
+++ organizer.sh
@@ -9,12 +9,42 @@ fi
 
 #PART 2: CREATE A TIMESTAMP STRING  
 # date command with the "+" FORMAT prints the current time where:
-# %Y = 4-dgit year
-# %h = month in words 
+# %Y = 4-digit year
+# %h = month in words and %m = month in numbers
 # %d = date
 # %H = hours 
 # %M = minutes
 # %S = seconds
-TIMESTAMP=$(date +"%Y-%h-%d  %H:%M:%S")
-echo "Timestamp: $TIMESTAMP"
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+TIMESTAMPS=$(date +"%Y-%h-%d  %H:%M:%S")
+echo "Timestamp: $TIMESTAMPS"
+
+#PART 3: REMOVE AND MOVE THE grades.csv INTO THE 'archive' FOLDER
+ORIGINAL_FILE="grades.csv"
+ARCHIVED_FILE="grades_${TIMESTAMP}.csv"
+
+if [ -f "$ORIGINAL_FILE" ];then 
+    mv "$ORIGINAL_FILE" archive/$ARCHIVED_FILE 
+    echo "The '$ORIGINAL_FILE' was successfully moved to 'archive/$ARCHIVED_FILE'"
+
+
+#PART 4: CREATE A NEW AND EMPTY FILE NAMED 'grades.csv' FOR THE NEXT BATCH OF GRADES
+    touch $ORIGINAL_FILE
+    echo "A new and empty '$ORIGINAL_FILE' was created" 
+
+#PART 5: LOG EVERYTHING INTO 'organizer.log'
+
+    echo "[$TIMESTAMP] archived '$ORIGINAL_FILE' as '$ARCHIVED_FILE'" >> organizer.log
+    echo "Logged this action to organizer.log"
+
+else
+    echo "There was no $ORIGINAL_FILE found in the current folder. There's nothing to archive" 
+    echo "[$TIMESTAMP] attempted to archive '$ORIGINAL_FILE' but it was missing." >> organizer.log
+fi 
+
+echo "DONE"
+
+
+
+
 
